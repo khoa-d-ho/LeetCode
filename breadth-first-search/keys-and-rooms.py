c9d8480fd4f90d8cef420161aa1ba15e07d3becc
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 11:47:41 -0400

    LC-keys-and-rooms [Runtime: 0 ms; Memory: 18.1 MB]

--- breadth-first-search/keys-and-rooms.py
+++ breadth-first-search/keys-and-rooms.py
@@ -1,19 +1,15 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        n = len(rooms)
-        node_visited = 0
-        queue = deque()
-        visited = [0] * n
-        queue.append(0)
-        visited[0] = 1
+        visited = set()
+        visited.add(0)
+        stack = [0]
 
-        while queue:
-            node = queue.popleft()
-            node_visited += 1
-            for neighbor in rooms[node]:
-                if not visited[neighbor]:
-                    visited[neighbor] = 1
-                    queue.append(neighbor)
+        while stack:
+            room_idx = stack.pop()
+            for key in rooms[room_idx]:
+                if key not in visited:
+                    visited.add(key)
+                    stack.append(key)
+        
+        return len(visited) == len(rooms)
 
-        return node_visited == n
-            
