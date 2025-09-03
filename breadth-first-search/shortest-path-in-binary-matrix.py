9b128dd9834df3559683d588bb142153b0a6210c
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 13:47:42 -0400

    LC-shortest-path-in-binary-matrix [Runtime: 207 ms; Memory: 19.6 MB]

--- breadth-first-search/shortest-path-in-binary-matrix.py
+++ breadth-first-search/shortest-path-in-binary-matrix.py
@@ -7,28 +7,20 @@ class Solution:
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
 
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if grid[i][j] == "*":
-        #             starting = (i, j)
-        #             break
-        #     if starting:
-        #         break
-
         queue.append((starting, 1))
         visited = {starting} # no duplicates
 
         while queue:
             coordinates, distance = queue.popleft()
-            i, j = coordinates[0], coordinates[1]
-            if i == n - 1 and j == n - 1:
+            r, c = coordinates[0], coordinates[1]
+            if r == n - 1 and c == n - 1:
                 return distance
             
             for di, dj in moves:
-                ci, cj = i + di, j + dj
-                if 0 <= ci < n and 0 <= cj < n and grid[ci][cj] == 0 and (ci, cj) not in visited:
+                cr, cc = r + di, c + dj
+                if 0 <= cr < n and 0 <= cc < n and grid[cr][cc] == 0 and (cr, cc) not in visited:
 
-                    visited.add((ci, cj))
-                    queue.append(((ci, cj), distance + 1))
+                    visited.add((cr, cc))
+                    queue.append(((cr, cc), distance + 1))
                     
         return -1
