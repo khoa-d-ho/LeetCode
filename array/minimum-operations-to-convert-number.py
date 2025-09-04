758a3331e13954a923ae8175be132469fab1a837
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 20:44:04 -0400

    LC-minimum-operations-to-convert-number [Runtime: N/A; Memory: N/A]

--- array/minimum-operations-to-convert-number.py
+++ array/minimum-operations-to-convert-number.py
@@ -1,16 +1,25 @@
-from collections import deque:
+from collections import deque
 
 class Solution:
     def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
-        if start == goal:
-            return 0
-        elif not nums:
+        if not nums:
             return -1
 
         queue = deque()
         queue.append((start, 0))
+        visited = {start}
 
         while queue:
             curr, ops = queue.popleft()
+            if curr == goal:
+                return ops
             for num in nums:
-                queue.append((curr + num, ops + 1))
+                operations = [curr + num, curr - num, curr ^ num]
+                for result in operations:
+                    if result == goal:
+                        return ops + 1
+                    if not (0 <= result <= 1000) or result in visited:
+                        break
+                    visited.add(result)
+                    queue.append((result, ops + 1))
+        return -1
