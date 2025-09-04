820ba1d4c766353b4dac9709b8c90dadfef2313d
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 21:05:05 -0400

    LC-min-stack [Runtime: N/A; Memory: N/A]

---
+++ stack/min-stack.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_val = float('inf')
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if val < self.min_val:
+            self.min_val = val
+        self.min_stack.append(self.min_val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
