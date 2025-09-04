4f94f73613251288abe99f9d76dfec7d33f9a6bd
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 21:24:48 -0400

    LC-simplify-path [Runtime: 1 ms; Memory: 17.7 MB]

--- string/simplify-path.py
+++ string/simplify-path.py
@@ -2,13 +2,14 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
         path = path.split("/")
+        invalid_names = ["", ".",'..']
         print(path)
         for item in path:
             # go back, must have item to go back
             if stack and item == "..":
                 stack.pop()
             # care .., since /../ = /
-            elif item not in ["", ".",'..']: 
+            elif item not in invalid_names: 
                 stack.append(item)
 
 
