0e210873a405167261dc07e53071abc6c6b9b7fd
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-03 23:00:13 -0400

    LC-meeting-rooms-ii [Runtime: 4 ms; Memory: 19.8 MB]

---
+++ array/meeting-rooms-ii.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # heapq.heapify
+        # heapq.heappop
+        # heapq.heappush
+        if not intervals:
+            return 0
+        
+        intervals.sort(key = lambda x: x[0])
+
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+            if start >= heap[0]:  # minheap
+                heapq.heappop(heap)
+            heapq.heappush(heap, end)
+        return len(heap)
