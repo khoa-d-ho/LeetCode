32ec7d7ca536a417e82ac4784b2f590a17decc27
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-09 19:59:21 -0400

    LC-car-pooling [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/car-pooling.py
+++ heap-priority-queue/car-pooling.py
@@ -4,9 +4,9 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         # Input: trips = [[2,1,5],[3,3,7]], capacity = 5
         # Output: true
-        # 1 2 3 4 5 6 7 timeline
-        # 2 2 5 5 5 3 3 passengers
-
+        # 1 2 3 4 5 6 7 8  9  10 11 timeline
+        # 0 3 3 7 7 3 3 10 10 10 10 passengers
+        #  +3  +4       -3+10
         # spots: capacity 0
         # heap = (5, 2) (7, 3)
         # next start = 3
@@ -32,8 +32,9 @@ class Solution:
         for i in range(1, len(trips)):
             p, s, e = trips[i][0], trips[i][1], trips[i][2]
             if s >= heap[0][0]:
-                total -= heap[0][1]
                 heapq.heappop(heap)
+            print(heap)
+            print(total)
             heapq.heappush(heap, (e, p))
             total += p
             
