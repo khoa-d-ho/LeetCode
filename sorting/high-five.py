ebce922d621fb155964db450ce3b1e1ba0d89153
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-09 15:33:51 -0400

    LC-high-five [Runtime: N/A; Memory: N/A]

--- sorting/high-five.py
+++ sorting/high-five.py
@@ -1,14 +1,24 @@
+import heapq
+
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        studs = {}
-        import heapq
-        for i, score in items:
-            studs[i] = studs.get(i, [])
-            heapq.heappush(studs[i], -score)
-
-        solution = []
-        for i in sorted(studs.keys()): 
-            top_five = [-heapq.heappop(studs[i]) for _ in range(5)]
-            average = sum(top_five) // 5
-            solution.append([i, average])
-        return solution
+        res_dict = {}
+        res = []
+        
+        for item in items:
+            idx, value = item[0], item[1]
+            if idx not in res_dict:
+                heap = []
+                heapq.heapify(heap)
+                res_dict[idx] = heap
+            heapq.heappush(res_dict[idx], value)
+            if len(res_dict[idx]) > 5:
+                heappop(res_dict[idx])
+            
+        for idx, heap in res_dict.items():
+            curr_list = [idx]
+            list(heap)
+            curr_list.append(int(sum(heap) / 5))
+            res.append(curr_list)
+        return res
+            
