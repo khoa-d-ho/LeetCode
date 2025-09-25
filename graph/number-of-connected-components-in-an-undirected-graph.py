82f0190f9388305b5becddd7f28d2a38e0a8633c
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-25 00:38:47 -0400

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: N/A; Memory: N/A]

---
+++ graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,21 @@
+class UnionFind:
+    def __init__(self, n): # n = 5
+        self.parent = [i for i in range(n)] # [0,0,0,2,4]
+
+    def find(self, x): # x = 2
+        if x != self.parent[x]:
+            return self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b): # 
+        self.parent[b] = self.parent[a]
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = UnionFind(n)
+
+        for u, v in edges:
+            graph.union(u, v)
+
+        sets = {graph.find(u) for u in range(n)}
+        return len(sets)
