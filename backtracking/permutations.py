ec6d81fb81cca3c3e43f6cc4abdf672697c5e9a8
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-24 21:59:09 -0400

    LC-permutations [Runtime: 3 ms; Memory: 17.9 MB]

--- backtracking/permutations.py
+++ backtracking/permutations.py
@@ -20,23 +20,3 @@ class Solution:
                 curr[first], curr[i] = curr[i], curr[first]
         backtrack(0, nums[:])
         return res
-
-
-# class Solution:
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         # 1 2 3
-#         # none, 1, 12, 123
-#         # 2, 23
-         
-#         res = []
-#         n = len(nums)
-
-#         def backtrack(idx, curr):
-#             res.append(curr[:]) # dont reference current list
-#             for i in range(idx, n):
-#                 curr.append(nums[i])
-#                 backtrack(i+1, curr)
-#                 curr.pop() # 12 13
-
-#         backtrack(0, [])
-#         return res
