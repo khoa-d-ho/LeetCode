abee8215d7b93a84e1360c5bf2587b9b98396145
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-25 01:17:18 -0400

    LC-the-earliest-moment-when-everyone-become-friends [Runtime: 4 ms; Memory: 18.4 MB]

--- sorting/the-earliest-moment-when-everyone-become-friends.py
+++ sorting/the-earliest-moment-when-everyone-become-friends.py
@@ -1,10 +1,11 @@
 class UnionFind:
     def __init__(self, n):
         self.parent = [i for i in range(n)]
+        self.size = [1] * n
 
     def find(self, x):
         if x != self.parent[x]:
-            return self.find(self.parent[x])
+            self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
     def union(self, a, b):
@@ -12,7 +13,14 @@ class UnionFind:
         rb = self.find(b)
         if ra == rb:
             return False
+        sa = self.size[ra]
+        sb = self.size[rb]
+        if sa < sb:
+            ra, rb = rb, ra
+        self.size[rb] += self.size[ra]
+        self.size[ra] = self.size[rb]
         self.parent[rb] = self.parent[ra]
+        
         return True
 
 # Input: logs = [[0,2,0],[1,0,1],[3,0,3],[4,1,2],[7,3,1]], n = 4
