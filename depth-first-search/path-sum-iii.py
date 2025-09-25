9b5773b98f83aaeab55894028de9a6406d8b7e7d
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-25 00:00:18 -0400

    LC-path-sum-iii [Runtime: 435 ms; Memory: 18.1 MB]

--- depth-first-search/path-sum-iii.py
+++ depth-first-search/path-sum-iii.py
@@ -6,26 +6,25 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root:
+            return 0
         self.res = 0
-        d = {0:1}
-        def dfs(root, prev_sum):
-            if not root:
+
+        def backtrack(curr_node, curr_sum):
+            if not curr_node:
+                return 
+            curr_sum += curr_node.val
+            if curr_sum == targetSum:
+                self.res += 1
+
+            backtrack(curr_node.left, curr_sum)
+            backtrack(curr_node.right, curr_sum)
+
+        def traverse(curr_node):
+            if not curr_node:
                 return
-
-            curr_sum = root.val + prev_sum           
-            target = curr_sum - targetSum
-            if target in d:
-                self.res += d[target]
-
-            if curr_sum in d:
-                d[curr_sum] += 1
-            else:
-                d[curr_sum] = 1
-
-
-            dfs(root.left, curr_sum)
-            dfs(root.right, curr_sum)
-            d[curr_sum] -= 1
-        
-        dfs(root, 0)
+            backtrack(curr_node, 0)
+            traverse(curr_node.left)
+            traverse(curr_node.right)
+        traverse(root)
         return self.res
