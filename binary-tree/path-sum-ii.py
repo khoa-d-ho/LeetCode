7123bbbb1e71edf6de08efaf7770f690d44fe64e
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-09-24 22:27:04 -0400

    LC-path-sum-ii [Runtime: 0 ms; Memory: 18.8 MB]

--- binary-tree/path-sum-ii.py
+++ binary-tree/path-sum-ii.py
@@ -10,24 +10,22 @@ class Solution:
             return []
         res = []
 
-        stack = [(root, [root.val], root.val)]
         # visited = {root} # only for graphs
 
-        while stack:
-            curr_node, curr_path, curr_sum = stack.pop()
-            if not curr_node.left and not curr_node.right and curr_sum == targetSum:
-                res.append(curr_path)
+        
+        def backtrack(curr_node, curr_path, curr_sum):
+            if not curr_node.left and not curr_node.right:
+                if curr_sum == targetSum:
+                    res.append(curr_path[:])
 
-            if curr_node.right:
-                new_path = list(curr_path)
-                new_path.append(curr_node.right.val)
-                new_sum = curr_sum + curr_node.right.val
-                stack.append((curr_node.right, new_path, new_sum))
             if curr_node.left:
-                new_path = list(curr_path)
-                new_path.append(curr_node.left.val)
-                new_sum = curr_sum + curr_node.left.val
-                stack.append((curr_node.left, new_path, new_sum))
+                curr_path.append(curr_node.left.val)
+                backtrack(curr_node.left, curr_path, curr_sum + curr_node.left.val)
+                curr_path.pop()
+            if curr_node.right:
+                next_path = curr_path.append(curr_node.right.val)
+                backtrack(curr_node.right, curr_path, curr_sum + curr_node.right.val)
+                curr_path.pop()
 
+        backtrack(root, [root.val], root.val)
         return res
-            
