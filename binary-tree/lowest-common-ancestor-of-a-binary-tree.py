eb868cc972083afe35ecea2d0b7739bcceae0c97
Khoa Ho <147029242+khoa-d-ho@users.noreply.github.com>
2025-10-08 00:05:48 -0400

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: 59 ms; Memory: 22.1 MB]

--- binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -5,18 +5,29 @@
 #         self.left = None
 #         self.right = None
 
+'''
+clarification: 
+- binary tree balanced? anything
+- 2 <= n <= 10^5
+- any two nodes with same val? no!
+
+'''
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
+        # recursion
+        # recurse to the subtree which has both p and q
+        # return curr_node if 
+            # p and q in diff subtrees
+            # OR either p or q is curr_node
+
+        if not root or root == q or root == p:
             return root
-
-        l = self.lowestCommonAncestor(root.left, p, q)
-        r = self.lowestCommonAncestor(root.right, p, q)
-
-        if l and r:
-            return root
-        else:
-            return r or l
-            
-
         
+        left_subtree = self.lowestCommonAncestor(root.left, p, q)
+        right_subtree = self.lowestCommonAncestor(root.right, p, q)
+
+        if left_subtree and right_subtree:
+            return root
+        
+        return left_subtree if left_subtree else right_subtree
